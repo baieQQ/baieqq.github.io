8143f5121df32bfd469f23b5e0b4f8c47bd8b10a
baieQQ <a076907753@gmail.com>
2022-08-30 16:24:52 +0800

    add download score_log

--- Children_lesson/keyboard_game/script.js
+++ Children_lesson/keyboard_game/script.js
@@ -1,4 +1,5 @@
 var start_game_run = false;
+let keydown_run = true;
 var id_list = [49, 50, 53, 56, 57, 48, 189, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191];
 var value_list = ['ㄅ', 'ㄉ', 'ㄓ', 'ㄚ', 'ㄞ', 'ㄢ', 'ㄦ', 'ㄆ', 'ㄊ', 'ㄍ', 'ㄐ', 'ㄔ', 'ㄗ', 'ㄧ', 'ㄛ', 'ㄟ', 'ㄣ', 'ㄇ', 'ㄋ', 'ㄎ', 'ㄑ', 'ㄕ', 'ㄘ', 'ㄨ', 'ㄜ', 'ㄠ', 'ㄤ', 'ㄈ', 'ㄌ', 'ㄏ', 'ㄒ', 'ㄖ', 'ㄙ', 'ㄩ', 'ㄝ', 'ㄡ', 'ㄥ'];
 
@@ -7,6 +8,7 @@ var last_keydown_correct = 0;
 var keydown_correct = 0;
 var score = 0;
 var game_time = 0;
+var game_score_log = [];
 var timer;
 
 function init(){
@@ -27,6 +29,10 @@ function game_start_reciprocal(){
     game_start_bool = true
     game_time = 0;
     score = 0;
+    let w = setInterval('owo', 1000);
+    for(let i = 0; i < w + 1; i++){
+        clearInterval(i);
+    }
     opo_id.textContent = '';
     time_id.textContent = game_time;
     score_id.textContent = score;
@@ -38,18 +44,17 @@ function game_start_reciprocal(){
     setTimeout(function(){reciprocal.textContent = "1"; music_play('reciprocal_1')}, time[4]);
     setTimeout(function(){reciprocal.textContent = "開始";  music_play('reciprocal_start')}, time[5]);
     setTimeout(function(){reciprocal.textContent = ""; start_game_run = true;}, time[6]);
-    setTimeout(function(){keydown_correct = game_id_rand(); game_time = 60; score = 0 }, time[6]);
+    setTimeout(function(){keydown_correct = game_id_rand(); game_time = 10; score = 0 }, time[6]);
     setTimeout(function(){setInterval(game_time_reciprocal, 1000); game_start_bool = false}, 6000);
 }
 
 function game_time_reciprocal(){
+    if(start_game_run == false)
+        return;
     let tmp_id = document.getElementById('time_id');
     if(game_time <= 0){
         start_game_run = false;
-        w = setInterval('owo', 1000);
-        for(let i = 1; i < w + 1; i++){
-            clearInterval(i);
-        }
+        game_score_log.push(score);
     }
     else{
         game_time -= 1;
@@ -86,17 +91,18 @@ function music_play(key_code){
 }
 
 function keyboard_keydown(e){
-    if(start_game_run == true){
+    if(start_game_run == true && keydown_run == true){
         if(!id_list.includes(e.keyCode)){
             return;
         }
+        console.log(keydown_run);
         var key_code = e.keyCode;
         var txt_id = document.getElementById(key_code);
         txt_id.style.fontSize = "100px";
         txt_id.style.color= "black";
         txt_id.style.zIndex = "9999";
         txt_id.style.webkitTextStroke = "1px white";
-        start_game_run = false;
+        keydown_run = false;
         music_play("wav_" + key_code);
         if(key_code == id_list[keydown_correct]){
             let tmp_id = document.getElementById('score_id') 
@@ -109,7 +115,7 @@ function keyboard_keydown(e){
                 txt_id.style.color="#eacd11";
                 txt_id.style.zIndex = "1";
                 txt_id.style.webkitTextStroke = ""; 
-                start_game_run = true
+                keydown_run = true
             }, 1000);
             
             setTimeout(function(){
@@ -124,11 +130,40 @@ function keyboard_keydown(e){
                 txt_id.style.color="#eacd11";
                 txt_id.style.zIndex = "1";
                 txt_id.style.webkitTextStroke = ""; 
-                start_game_run = true
+                keydown_run = true
             }, 500);
         }
     }
 }
 
+function getScoreLog(){
+    let score_txt = "";
+    for(let i = 0; i < game_score_log.length; i++){
+        score_txt += '第 ' + (i + 1) + ' 次成績為：' + game_score_log[i] + '\n';
+    }
+    console.log(score_txt);
+    return score_txt;
+}
+
+function downloadFile() {
+    //藉型別陣列建構的 blob 來建立 URL
+    let fileName = "fileName.txt";
+    const data = getScoreLog();
+    let blob = new Blob([data], {
+      type: "application/octet-stream",
+    });
+    var href = URL.createObjectURL(blob);
+    // 從 Blob 取出資料
+    var link = document.createElement("a");
+    document.body.appendChild(link);
+    link.href = href;
+    link.download = fileName;
+    link.click();
+  }
+
+
 var body = document.body;
 body.addEventListener('keydown', keyboard_keydown ,false) //偵測按下按鍵的行為
+
+let downloadBtn = document.querySelector(".downloadBtn");
+downloadBtn.addEventListener("click", downloadFile);
