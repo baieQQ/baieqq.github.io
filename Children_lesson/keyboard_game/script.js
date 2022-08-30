d4d00c9965a7248d761e5281bfd481e044ae808a
baieQQ <a076907753@gmail.com>
2022-08-30 16:36:03 +0800

    add download

--- Children_lesson/keyboard_game/script.js
+++ Children_lesson/keyboard_game/script.js
@@ -1,7 +1,10 @@
 var start_game_run = false;
 let keydown_run = true;
+let error_keydown = 0;
+let correct_keydown = 0;
 var id_list = [49, 50, 53, 56, 57, 48, 189, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191];
 var value_list = ['ㄅ', 'ㄉ', 'ㄓ', 'ㄚ', 'ㄞ', 'ㄢ', 'ㄦ', 'ㄆ', 'ㄊ', 'ㄍ', 'ㄐ', 'ㄔ', 'ㄗ', 'ㄧ', 'ㄛ', 'ㄟ', 'ㄣ', 'ㄇ', 'ㄋ', 'ㄎ', 'ㄑ', 'ㄕ', 'ㄘ', 'ㄨ', 'ㄜ', 'ㄠ', 'ㄤ', 'ㄈ', 'ㄌ', 'ㄏ', 'ㄒ', 'ㄖ', 'ㄙ', 'ㄩ', 'ㄝ', 'ㄡ', 'ㄥ'];
+
 
 var game_start_bool = false;
 var last_keydown_correct = 0;
@@ -9,6 +12,8 @@ var keydown_correct = 0;
 var score = 0;
 var game_time = 0;
 var game_score_log = [];
+var game_keydown_error = [];
+var game_keydown_correct = [];
 var timer;
 
 function init(){
@@ -29,6 +34,8 @@ function game_start_reciprocal(){
     game_start_bool = true
     game_time = 0;
     score = 0;
+    error_keydown = 0;
+    correct_keydown = 0;
     let w = setInterval('owo', 1000);
     for(let i = 0; i < w + 1; i++){
         clearInterval(i);
@@ -44,7 +51,7 @@ function game_start_reciprocal(){
     setTimeout(function(){reciprocal.textContent = "1"; music_play('reciprocal_1')}, time[4]);
     setTimeout(function(){reciprocal.textContent = "開始";  music_play('reciprocal_start')}, time[5]);
     setTimeout(function(){reciprocal.textContent = ""; start_game_run = true;}, time[6]);
-    setTimeout(function(){keydown_correct = game_id_rand(); game_time = 10; score = 0 }, time[6]);
+    setTimeout(function(){keydown_correct = game_id_rand(); game_time = 30; score = 0 }, time[6]);
     setTimeout(function(){setInterval(game_time_reciprocal, 1000); game_start_bool = false}, 6000);
 }
 
@@ -55,6 +62,8 @@ function game_time_reciprocal(){
     if(game_time <= 0){
         start_game_run = false;
         game_score_log.push(score);
+        game_keydown_correct.push(correct_keydown);
+        game_keydown_error.push(error_keydown);
     }
     else{
         game_time -= 1;
@@ -107,6 +116,7 @@ function keyboard_keydown(e){
         if(key_code == id_list[keydown_correct]){
             let tmp_id = document.getElementById('score_id') 
             last_keydown_correct = keydown_correct;
+            correct_keydown += 1;
             score += 100;
             tmp_id.textContent = score;
             game_image(key_code); 
@@ -125,6 +135,7 @@ function keyboard_keydown(e){
             }, 1000);
         }
         else{
+            error_keydown += 1;
             setTimeout(function(){
                 txt_id.style.fontSize = "24px";
                 txt_id.style.color="#eacd11";
@@ -139,7 +150,7 @@ function keyboard_keydown(e){
 function getScoreLog(){
     let score_txt = "";
     for(let i = 0; i < game_score_log.length; i++){
-        score_txt += '第 ' + (i + 1) + ' 次成績為：' + game_score_log[i] + '\n';
+        score_txt += '第 ' + (i + 1) + ' 次成績為：' + game_score_log[i] + '，正確點擊次數為：' + game_keydown_correct[i] + '，錯誤次數為：' + game_keydown_error[i] + '\n';
     }
     console.log(score_txt);
     return score_txt;
@@ -147,7 +158,7 @@ function getScoreLog(){
 
 function downloadFile() {
     //藉型別陣列建構的 blob 來建立 URL
-    let fileName = "fileName.txt";
+    let fileName = "ScoreLog.txt";
     const data = getScoreLog();
     let blob = new Blob([data], {
       type: "application/octet-stream",
